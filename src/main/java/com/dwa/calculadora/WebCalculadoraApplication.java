f09fbd0e11af4e0eab372a9d44e6eab50dec872f
Jose Rodriguez Juarez <joserj99.rj@gmail.com>
2021-05-24 14:05:57 -0500

    Charge...

---
+++ src/main/java/com/dwa/calculadora/WebCalculadoraApplication.java
@@ -0,0 +1,13 @@
+package com.dwa.calculadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCalculadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCalculadoraApplication.class, args);
+	}
+
+}
